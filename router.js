413b3ea95212bdbeb5064d7ff81b037a04232f06
jefferson <jeffersondesouzacordel@gmail.com>
2016-11-07 23:01:37 -0300

    Autenticação que impede usuarios nao logados de aessar paginas de admnistrador feita

--- router.js
+++ router.js
@@ -11,47 +11,68 @@ Router.route('/', function(){
 
 
     Router.route('/adicionar-jogador',function(){
-        console.log(Meteor.userId());
 
-    	this.render('adicionar_jogador',{
-    		to: 'main'
-    	});
+        if(Meteor.userId()){
+            this.render('adicionar_jogador',{
+                to: 'main'
+            });
+        }else{
+            this.render('erro');
+        }
+
     });
 
     Router.route('/lista-jogadores',function(){
-    	this.render('jogadores_lista',{
-    		to: 'main'
-    	});
+        if(Meteor.userId()){
+        	this.render('jogadores_lista',{
+        		to: 'main'
+        	});
+        }else{
+            this.render('erro');
+        }
     })
 
 
     Router.route('/salvar-sorteio', function(){
-        this.render('salvar_sorteio_template',{
-            to:'main'
-        })
+        if(Meteor.userId()){
+            this.render('salvar_sorteio_template',{
+                to:'main'
+            });
+        }else{
+            this.render('erro');
+        }
     })
 
     Router.route('/jogadores/:_id',function(){
 
-        this.render('jogadores_detalhes', {
-    		to:"main",
-    		data:function(){
-    			//Meteor.subscribe("websites", Session.get("searchValue"));
+        if(Meteor.userId()){
+            this.render('jogadores_detalhes', {
+        		to:"main",
+        		data:function(){
+        			//Meteor.subscribe("websites", Session.get("searchValue"));
 
 
-    			return Jogadores.findOne({_id:this.params._id});
-    		}
-    	});
+        			return Jogadores.findOne({_id:this.params._id});
+        		}
+        	});
+        }else{
+            this.render('erro');
+        }
     });
 
 
     Router.route('/jogadores/:_id/edit',function(){
 
-        this.render('adicionar_jogador', {
-    		to:"main",
-    		data:function(){
-    			//Meteor.subscribe("websites", Session.get("searchValue"));
-    			return Jogadores.findOne(this.params._id);
-    		}
-    	});
+        if(Meteor.userId()){
+            this.render('adicionar_jogador', {
+                to:"main",
+                data:function(){
+                    //Meteor.subscribe("websites", Session.get("searchValue"));
+                    return Jogadores.findOne(this.params._id);
+                }
+            });
+
+        }else{
+            this.render('erro');
+        }
     })
